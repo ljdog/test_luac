a0e4b8bff8bdc9c0889c6d5f2d82d5e2bbf5f0ac
郭崇强 <guochongqiang@dartou.com>
2019-09-29 21:31:17 +0800

    添加遗漏的文件

---
+++ lua_call_c/my_addtest.c
@@ -0,0 +1,24 @@
+//加载lua头文件
+#include <lauxlib.h>
+
+static int add(lua_State *L)
+{
+    float a,b;
+    a = lua_tonumber(L, 1);//第一个加数，函数的第一个参数总是索引1
+    b = lua_tonumber(L, 2);
+    lua_pushnumber(L, a + b);
+    return 1;   //返回1表示该方法只有一个返回值
+}
+
+static const struct luaL_Reg mylib_funcs[] = {
+    {"add_c",add},
+    {NULL,NULL}
+};
+
+int luaopen_mylib (lua_State *L)
+{
+    luaL_newlib(L,mylib_funcs);
+    return 1;
+}
+
+//gcc  -o my_addtest.o -c my_addtest.c ; gcc -O2 -bundle -undefined dynamic_lookup -o mylib.so my_addtest.o 
---
+++ lua_call_c/test_so.lua
@@ -0,0 +1,2 @@
+local mylib = require "mylib"
+print(mylib.add_c(1000.32,2))
