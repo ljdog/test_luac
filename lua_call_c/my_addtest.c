56ffdeeb700cda45cf6173c23b77ff96dd5f4718
郭崇强 <guochongqiang@dartou.com>
2020-04-26 19:53:51 +0800

    暂存

--- lua_call_c/my_addtest.c
+++ lua_call_c/my_addtest.c
@@ -1,7 +1,7 @@
 //加载lua头文件
 #include <lauxlib.h>
 
-static int add(lua_State *L)
+static int add(lua_State *L) //int 为必须
 {
     float a,b;
     a = lua_tonumber(L, 1);//第一个加数，函数的第一个参数总是索引1
@@ -11,11 +11,11 @@ static int add(lua_State *L)
 }
 
 static const struct luaL_Reg mylib_funcs[] = {
-    {"add_c",add},
+    {"add_c",add}, //add_c  为导出到lua 用的函数名
     {NULL,NULL}
 };
 
-int luaopen_mylib (lua_State *L)
+int luaopen_mylib (lua_State *L) // mylib so的名称
 {
     luaL_newlib(L,mylib_funcs);
     return 1;
---
+++ test.lua
@@ -0,0 +1,12 @@
+function printmsg()
+    print("hello world")
+end
+
+function add(a,b)
+    return a+b
+end
+
+function add_tb(data)
+    return data.a + data.b
+end
+x = 10
